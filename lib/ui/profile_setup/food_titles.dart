a5a761b9abd8d4b351878628b4a2f3644aaf9aa2
ntua-el20026 <72971284+ntua-el20026@users.noreply.github.com>
2024-01-10 15:24:24 +0200

    Delete lib directory
    Corrected to lousimo mou

--- lib/ui/profile_setup/food_titles.dart
+++
@@ -1,8 +0,0 @@
-List<String> allTitles (int category) {
-  switch(category) {
-    case 7:
-      return ['Keftedes', 'Stifado', 'Souvlaki', 'Briam', 'Tzatziki', 'Fava', 'Fish', 'Eggplant']; 
-    default: 
-      return [];
-  }
-}
